0b71f7be8b82ce7f029b2819c6dd183c78a5ea12
Henry Ragua <henryragua@gmail.com>
2025-08-05 08:25:56 -0500

    se desarrolla la capa de infra del proyecto

--- src/user/infraestructure/user.controller.ts
+++ src/user/infraestructure/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserCreator } from 'src/user/application/user-creator/UserCreator';
+import { GetAllUsers } from '../application/get-all-users/GetAllUsers';
+import { CreateUserDto } from './dto/create-user.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    private readonly userCreator: UserCreator,
+    private readonly getAllUsers: GetAllUsers,
+  ) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userCreator.execute({
+      ...createUserDto,
+      id: uuidv4(),
+    });
+  }
+
+  @Get()
+  findAll() {
+    return this.getAllUsers.execute();
+  }
+}
